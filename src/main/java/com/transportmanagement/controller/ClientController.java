1c32950d2747b2b8ab3b4af69269c5f9b6cd1b45
thucnguyen15699 <thucnguyen1506@gmail.com>
2025-10-26 21:23:32 +0700

    edit upload file

--- src/main/java/com/transportmanagement/controller/ClientController.java
+++ src/main/java/com/transportmanagement/controller/ClientController.java
@@ -5,12 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.transportmanagement.dto.ClientBranchRequestDto;
 import com.transportmanagement.dto.ClientRequestDto;
@@ -36,10 +38,18 @@ public class ClientController {
 	@Autowired
 	private ClientResource clientResource;
 
+//	@PostMapping("/add")
+//	@Operation(summary = "Api to add client")
+//	public ResponseEntity<CommonApiResponse> addClient(ClientRequestDto request) {
+//		return clientResource.addClient(request);
+//	}
+	
 	@PostMapping("/add")
 	@Operation(summary = "Api to add client")
-	public ResponseEntity<CommonApiResponse> addClient(ClientRequestDto request) {
-		return clientResource.addClient(request);
+	public ResponseEntity<CommonApiResponse> addClient(
+	        @ModelAttribute ClientRequestDto request
+	) {
+	    return clientResource.addClient(request);
 	}
 
 	@PutMapping("/document/udpate")
